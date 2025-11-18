74d17dc647fe58667f4c4b039f9f6791fcc2bf0b
Richard Kovacs <rkovacs@leroylogistique.com>
2025-11-18 17:24:36 +0100

    feat: implement landing page with navigation and skills showcase

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,49 @@
+<script setup lang="ts">
+import type { NavigationMenuItem } from "@nuxt/ui";
+
+const items = ref<NavigationMenuItem[]>([
+  [
+    {
+      label: "Home",
+      to: "/",
+    },
+    {
+      label: "Skills",
+      to: "/skills",
+    },
+    {
+      label: "Projects",
+      to: "/projects",
+    },
+    {
+      label: "Experiences",
+      to: "/experiences",
+    },
+  ],
+]);
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <UApp>
+    <UHeader>
+      <template #title>
+        <NuxtLink to="/" class="font-bold">Hello World</NuxtLink>
+      </template>
+
+      <UNavigationMenu
+        :items="items"
+        :ui="{
+          root: 'border-black',
+        }"
+      />
+
+      <template #body>
+        <UNavigationMenu :items="items" orientation="vertical" />
+      </template>
+    </UHeader>
+
+    <UMain>
+      <NuxtPage />
+    </UMain>
+  </UApp>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+import tailwindcss from "@tailwindcss/vite";
 
-  modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/image',
-    '@nuxt/ui'
-  ]
-})
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  vite: { plugins: [tailwindcss()] },
+  css: ["./app/assets/css/main.css"],
+  modules: ["@nuxt/content", "@nuxt/eslint", "@nuxt/image", "@nuxt/ui"],
+  ui: {
+    colorMode: false,
+  },
+});
