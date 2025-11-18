f0cf147da54c75f83df5d54f9ab4759b7d538db0
Richard Kovacs <rkovacs@leroylogistique.com>
2025-11-18 15:51:09 +0100

    chore: clear of next.js project

--- src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export const Footer = () => {
-  return (
-    <footer className="flex flex-col items-center justify-center w-full gap-2 py-8 text-center">
-      <p>Designed and built by Richard Kovacs</p>
-      <p className="opacity-75">© Copyright 2024 - Richard Kovacs & B34R.-</p>
-    </footer>
-  );
-};
